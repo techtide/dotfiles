55deba101e45bbfe36f8d4412d42e1869b9a977a
Arman <techtide@users.noreply.github.com>
2017-12-26 16:18:02 +0000

    Update screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# I may switch to Sway w/ Wayland so this would render unfunctional, hence my decision to put it off for a bit.
 read -p "Take screenshot of external display (0) or the laptop screen (1): " choice
 read -p "File name: " name
 if [$choice -ge 0];then
